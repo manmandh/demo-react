45f90ca0782e6b8ae5a4c62233833242d378e02e
Marry <mantm.22ite@vku.udn.vn>
2023-05-25 17:08:08 +0700

    first commit

---
+++ app/mantran/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main className="block w-screen h-screen text-xl text-red-600 bg-white text-center">
+        <p className="p-4 m-2">hello man</p>
+      </main>
+    )
+  }
+  
